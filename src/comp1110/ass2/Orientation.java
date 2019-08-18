6cabdbe372863f1c402a4a60453e7ddd06aef95e
Nicholas Russell <nicholas-russell@users.noreply.github.com>
2019-08-18 19:26:59 +1000

    Converted Orientation enum declaration to upper case

--- src/comp1110/ass2/Orientation.java
+++ src/comp1110/ass2/Orientation.java
@@ -4,17 +4,12 @@ public enum Orientation {
     Zero, One, Two, Three;
 
     //Translate the orientation to number
-    public int toInt(){
-
-
-    }
+    public int toInt(){ return 0; };
 
     /*
      * Translate the orientation to the string, used for debugging.
      * For example, Zero is equal to "North", One is equal to "West" and so on.
      */
     @Override
-    public String toString(){
-
-    }
+    public String toString(){ return null; }
 }
