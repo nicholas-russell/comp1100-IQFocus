f7f22c53e19bcc11ccc6d8028a449e600860a106
Matthew <u6951444@anu.edu.au>
2019-08-18 15:21:19 +1000

    ..

--- src/comp1110/ass2/gui/Board.java
+++ src/comp1110/ass2/gui/Board.java
@@ -27,15 +27,44 @@ public class Board extends Application {
 
      */
     // FIXME Task 8: Implement challenges (you may use challenges and assets provided for you in comp1110.ass2.gui.assets: sq-b.png, sq-g.png, sq-r.png & sq-w.png)
-    public
+    public String challangeEncoding(String challangeSquare) {
+        char[] encodingArray = challangeSquare.toCharArray();
 
 
+        //Example of what encoding would look like, also need to covert Char to Variable Colour, B -> Blue, R -> Red
+        Square 0 = encodingArray[0];
+        ......
+
+    }
+
+    /* Implementing Challanges from TestUtility.
+    *How encoding the objective central 9 squares work. The Objective 3x3 square is split into the 3 rows. Starting from
+    * 0, each square is encoded with a number for example 0, then the square to the right is encoded with the next
+    * corresponding number e.g. 1. So the encoding for the 3x3 objective square will look like this,
+    *
+    *                         [0][1][2]
+    *                         [3][4][5]
+    *                         [6][7][8]
+    *
+    * Then we can implement challenges by assigning colours to the corresponding squares of the objective square.
+    *
+    *       This encoding design is provided in the Assets folder under the file challenge_encoding.png
+    *
+
+
+     */
 
     // FIXME Task 10: Implement hints
-    public enum KeyInput
-    /*
+    public class user
 
-
+    /* When the User holds down the "/" key, they arte suppose to "see" one or more pieces they can play to help them
+    * towards the solution! Javafx
+    * This can be done is a number of possible way, the easiest method would be to simply return in the interface the
+    * letter of the corresponding piece/s such as A, G or D.
+    *
+    *Second Method would be to somehow highlight the piece, outlining or distinguishing it from the other pieces.
+    *
+    *
     */
 
     // FIXME Task 11: Generate interesting challenges (each challenge may have just one solution)
