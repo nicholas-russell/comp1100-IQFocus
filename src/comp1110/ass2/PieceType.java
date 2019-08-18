7591fc7f69d6b724873410b82709556814205dc7
Nicholas Russell <nicholas-russell@users.noreply.github.com>
2019-08-18 19:48:29 +1000

    Updated documentation, renamed method to camelCase

--- src/comp1110/ass2/PieceType.java
+++ src/comp1110/ass2/PieceType.java
@@ -3,18 +3,22 @@ package comp1110.ass2;
 public enum PieceType {
     A,B,C,D,E,F,G,H,I,J;
 
-    /*
+    /**
      * Build a colormap has 10 arrays, each array represents a PieceType.
-     * In each array, there are 4*3 = 12 states(All PieceType can be put in a 4*3 rectangle).
-     * The state will be the four color or Null(not on the piece).
+     * In each array, there are 4*4 = 16 states(All PieceType can be put in a 4*4 rectangle).
+     * The state will be one of the four colors in State or Empty.
      */
-    private static State[][] colormap = new State[10][12];
+    private static State[][] colorMap = new State[10][16];
 
-   /*
+   /**
     * Give x-offset, y-offset and the orientation of the piece, return a state on the piece.
     * If the position is not on the piece, return null.
+    *
+    * @param x The x offset on the piece for the part you want to find
+    * @param y The y offset on the piece for the part you want to find
+    * @param orientation The orientation of the piece
     */
-    public State GetStateOnPiece(int x, int y, Orientation orientation) {
+    public State getStateOnPiece(int x, int y, Orientation orientation) {
         return null;
     }
 
