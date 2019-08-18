0b2eae8a44753b539ab447ebc744042bc1c5dd3b
Matthew <u6951444@anu.edu.au>
2019-08-18 14:47:21 +1000

    ..

--- src/comp1110/ass2/gui/Board.java
+++ src/comp1110/ass2/gui/Board.java
@@ -10,17 +10,33 @@ public class Board extends Application {
     private static final int BOARD_HEIGHT = 700;
 
     // FIXME Task 7: Implement a basic playable Focus Game in JavaFX that only allows pieces to be placed in valid places
-    public boolean isPlacementValid (String dataForPiecePlacement){
+    public boolean isPlacementValid (String placement){
 
 
     }
-    /* Firstly Need Another Class for State, which Yuhui has created.
-     * This Function Should take in a
+    /* Firstly needs the usage of another Class for State, which Yuhui has created in his branch.
+
+     * isPLacementValid should intake a placement string with data related to a piece being placed. This method will be
+     * mainly made up of if cases which checks that all the slots the piece is being placed in are a State value of empty
+     * ;returning false. Otherwise returns true for non-valid placements.
+     *
+     * Each individual piece, a,b,c,d... will need
+     * its own if case as the pieces are all unique shapes.
+     *
+     *Note:This method is similar in nature to the one performed in Task 6 of Assignment 1
 
      */
     // FIXME Task 8: Implement challenges (you may use challenges and assets provided for you in comp1110.ass2.gui.assets: sq-b.png, sq-g.png, sq-r.png & sq-w.png)
+    public
+
+
 
     // FIXME Task 10: Implement hints
+    public enum KeyInput
+    /*
+
+
+    */
 
     // FIXME Task 11: Generate interesting challenges (each challenge may have just one solution)
 
