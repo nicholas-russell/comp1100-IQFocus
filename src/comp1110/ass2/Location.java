faf7caf656b4467ac1a03a8c9dbc188670ff747e
Nicholas Russell <nicholas-russell@users.noreply.github.com>
2019-08-18 20:00:23 +1000

    Created Location class

---
+++ src/comp1110/ass2/Location.java
@@ -0,0 +1,15 @@
+package comp1110.ass2;
+
+public class Location {
+    private int X;
+    private int Y;
+
+    public Location(int x, int y) {
+        this.X = x;
+        this.Y = y;
+    }
+
+    public int getX() {return X;}
+    public int getY() {return Y;}
+
+}
