50fe3a8c7595587ab94ef31c1c3b1ef358eddccf
Nick Russell <u6963514@anu.edu.au>
2019-08-18 16:35:30 +1000

    Fixed spelling error

--- src/comp1110/ass2/Challenge.java
+++ src/comp1110/ass2/Challenge.java
@@ -3,13 +3,13 @@ package comp1110.ass2;
 public class Challenge {
     public int diffculty; // Set the diffculty for challanges(Task 11)
 
-    //Update the boardstate according to the challange given
-    public void addChallengeToBoard(String challange) {
+    //Update the boardstate according to the challenge given
+    public void addChallengeToBoard(String challenge) {
 
     }
 
-    //Decide the difficulty for a given challange
-    public int decideDifficulty(String challange) {
+    //Decide the difficulty for a given challenge
+    public int decideDifficulty(String challenge) {
 
     }
 
