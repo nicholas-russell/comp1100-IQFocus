dc33a1ded09b4fe96751020ac681b9d9b4d2acea
Nicholas Russell <nicholas-russell@users.noreply.github.com>
2019-08-21 22:40:42 +1000

    Added compass direction identifiers to Orientation type, and set a constructor.

--- src/comp1110/ass2/Orientation.java
+++ src/comp1110/ass2/Orientation.java
@@ -1,7 +1,13 @@
 package comp1110.ass2;
 
 public enum Orientation {
-    Zero, One, Two, Three;
+    Zero("North"), One("West"), Two("South"), Three("East");
+
+    private String compassOrientation;
+
+    private Orientation(String compassOrientation) {
+        this.compassOrientation = compassOrientation;
+    }
 
     /** Translate the orientation to number */
     public int toInt(){ return 0; };
