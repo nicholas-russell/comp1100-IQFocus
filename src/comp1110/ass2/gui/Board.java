1d27e66776965dd5076fc5d35cd0177c27332755
Nicholas Russell <nicholas-russell@users.noreply.github.com>
2019-09-17 10:13:21 +1000

    Started task 7 in Board class

--- src/comp1110/ass2/gui/Board.java
+++ src/comp1110/ass2/gui/Board.java
@@ -1,15 +1,48 @@
 package comp1110.ass2.gui;
 
-import comp1110.ass2.Location;
-import comp1110.ass2.State;
+import comp1110.ass2.*;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Board extends Application {
 
+    /* board layout */
+    private static final double SQUARE_SIZE = 70;
+    private static final int VIEWER_WIDTH = 720;
+    private static final int VIEWER_HEIGHT = 520;
+    private static final int BOARD_MARGIN_TOP = 87;
+    private static final int BOARD_MARGIN_LEFT = 41;
+    private static final int BOARD_MARGIN_RIGHT = 43;
+    private static final int BOARD_MARGIN_BOTTOM = 25;
+    private static final double SQUARE_SCALE_FACTOR = 0.70;
     private static final int BOARD_WIDTH = 933;
-    private static final int BOARD_HEIGHT = 700;
+    private static final int BOARD_HEIGHT = 650;
+    private int BOARD_X;
+    private int BOARD_Y;
+
+    private static final String URI_BASE = "assets/";
+
+    private final Group root = new Group();
+    private final Group controls = new Group();
+    private Pane boardPieces = new Pane();
+    private Pane board = new Pane();
+    private Pane errors = new Pane();
+    private Pane controlPieces = new Pane();
 
     /* challangeSquare is a 9 character String, with each character corresponding to the
     state of one of the squares that makes up the central 3x3 challange square
@@ -27,24 +60,6 @@ public class Board extends Application {
     public State getStateFromLocation (Location location) {
         return null;
     }
-
-    // FIXME Task 7: Implement a basic playable Focus Game in JavaFX that only allows pieces to be placed in valid places
-    public boolean isPlacementValid (String placement){
-        return false;
-
-    }
-    /* Firstly needs the usage of another Class for State, which Yuhui has created in his branch.
-
-     * isPlacementValid should intake a placement string with data related to a piece being placed. This method will be
-     * mainly made up of if cases which checks that all the slots the piece is being placed in are a State value of empty
-     * ;returning false. Otherwise returns true for non-valid placements.
-     *
-     * Each individual piece, a,b,c,d... will need
-     * its own if case as the pieces are all unique shapes.
-     *
-     *Note:This method is similar in nature to the one performed in Task 6 of Assignment 1
-
-     */
 
     // FIXME Task 8: Implement challenges (you may use challenges and assets provided for you in comp1110.ass2.gui.assets: sq-b.png, sq-g.png, sq-r.png & sq-w.png)
     public String challengeEncoding (String challenge, String boardState) {
@@ -91,14 +106,14 @@ public class Board extends Application {
     }
 
 
-    /* When the User holds down the "/" key, they are suppose to "see" one or more pieces they can play to help them
+    /* When the User holds down the "/" key, they are suppose to "see" one or more boardPieces they can play to help them
     * towards the solution!
      * Used in tandem with Javafx method found on line 131
      *
     * The easiest method would be to simply return in the interface the letter of the corresponding piece/s such as A,
     * G or D.
     *
-    * Second Method would be to somehow highlight the piece, outlining or distinguishing it from the other pieces.
+    * Second Method would be to somehow highlight the piece, outlining or distinguishing it from the other boardPieces.
     *
     *
     */
@@ -135,11 +150,215 @@ public class Board extends Application {
         * available for a given challenge square.
          */
     }
+
+
+    /**
+     *  Returns JavaFX Text object for error, styled to be red and bold.
+     * @param text String that should be displayed for error.
+     * @return JavaFX Text object styled as error.
+     */
+    private Text getErrorText(String text) {
+        Text error = new Text("ERROR: " + text);
+        error.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
+        error.setFill(Color.RED);
+        return error;
+    }
+
+    /**
+     * Draws error box onto viewer
+     * @param err Error string
+     */
+    private void drawErrorBox(String err) {
+        HBox errorBox = new HBox();
+        errorBox.getChildren().add(getErrorText(err));
+        errorBox.setAlignment(Pos.CENTER);
+        errorBox.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+        errorBox.setLayoutX(0);
+        errorBox.setLayoutY(0);
+        errors.getChildren().add(errorBox);
+    }
+
+    /**
+     * Gives an array of Piece objects from a placement string
+     * @param placement Placement string
+     * @return Array of Piece's from placement string
+     */
+    private Piece[] getPiecesFromPlacement(String placement) {
+        if (!FocusGame.isPlacementStringWellFormed(placement)) {
+            return null;
+        }
+        int numberOfPieces = placement.length()/4;
+        int i = 0;
+        int pIndex = 0;
+        Piece[] pieces = new Piece[numberOfPieces];
+        while (i < numberOfPieces*4) {
+            pieces[pIndex] = new Piece(placement.substring(i,i+4));
+            i += 4;
+            pIndex++;
+        }
+        return pieces;
+    }
+
+    /**
+     * Returns x & y offset values depending on PieceType and Orientation.
+     * These are multiplied by the SQUARE_SIZE to give a real pixel value.
+     * @param pieceType The PieceType of the piece being drawn
+     * @param orientation The Orientation of the piece being drawn
+     * @return Array of two doubles, the first for the x offset and the second for the y offset.
+     */
+    private double[] getOrientationOffsets(PieceType pieceType, Orientation orientation) {
+        double[] offsets = new double[]{0, 0};
+        switch (orientation) {
+            case One:
+                switch (pieceType) {
+                    case A:
+                    case D:
+                    case E:
+                    case G:
+                        offsets[0] = -0.5;
+                        offsets[1] = 0.5;
+                        break;
+                    case B:
+                    case C:
+                    case F:
+                    case J:
+                        offsets[0] = -1;
+                        offsets[1] = 1;
+                        break;
+                    case I:
+                    case H:
+                        break;
+                }
+                break;
+            case Two:
+                switch (pieceType) {
+                    case A:
+                    case B:
+                    case C:
+                    case D:
+                    case E:
+                    case F:
+                    case G:
+                    case H:
+                    case I:
+                    case J:
+                        offsets[0] = 0;
+                        offsets[1] = 0;
+                        break;
+                }
+                break;
+            case Three:
+                switch (pieceType) {
+                    case A:
+                    case D:
+                    case E:
+                    case G:
+                        offsets[0] = -0.5;
+                        offsets[1] = 0.5;
+                        break;
+                    case F:
+                    case J:
+                        offsets[0] = -1;
+                        offsets[0] = 1;
+                        break;
+                    case H:
+                    case I:
+                        break;
+                    case B:
+                    case C:
+                        offsets[0] = -1;
+                        offsets[1] = 1;
+                        break;
+                }
+                break;
+            case Zero:
+            default:
+                break;
+        }
+        return offsets;
+    }
+
+
+    /**
+     * Gets JavaFX ImageView object for a piece and scales it.
+     * @param p The PieceType
+     * @return Scaled ImageView of the piece object.
+     */
+    private ImageView getImageFromFile(PieceType p) {
+        InputStream pieceFile = getClass().getResourceAsStream(URI_BASE + p.toString().toLowerCase() + ".png");
+        Image pieceImage = new Image(pieceFile);
+        double imageHeight = pieceImage.getHeight();
+
+        ImageView pieceImageView = new ImageView(pieceImage);
+        pieceImageView.setFitHeight(SQUARE_SCALE_FACTOR*imageHeight);
+        pieceImageView.setPreserveRatio(true);
+        return pieceImageView;
+    }
+
+    /**
+     * Generates ImageViews for all Piece's in an array
+     * @param pieceList The array of Piece's to be generated
+     * @return An array of ImageView's to display
+     */
+    private ImageView[] getImageFromPiece(Piece[] pieceList) {
+        ImageView[] images = new ImageView[pieceList.length];
+        int i = 0;
+        for (Piece p : pieceList) {
+            images[i] = getImageFromFile(p.getPieceType());
+            double[] offsets = getOrientationOffsets(p.getPieceType(), p.getOrientation());
+            double xPos = BOARD_MARGIN_LEFT+p.getLocation().getX()*SQUARE_SIZE;
+            double yPos = BOARD_MARGIN_TOP+p.getLocation().getY()*SQUARE_SIZE;
+            int angle = p.getOrientation().toInt()*90;
+            images[i].setRotate(angle);
+            images[i].setX(xPos+(SQUARE_SIZE*offsets[0]));
+            images[i].setY(yPos+(SQUARE_SIZE*offsets[1]));
+            i++;
+        }
+        return images;
+    }
+
+    void makePlacement(String placement) {
+        errors.getChildren().clear();
+        boardPieces.getChildren().clear();
+        if (!FocusGame.isPlacementStringWellFormed(placement)) {
+            drawErrorBox("Placement string not valid");
+            return;
+        }
+        Piece[] pieceList = getPiecesFromPlacement(placement);
+        ImageView[] imageList = getImageFromPiece(pieceList);
+        boardPieces.getChildren().addAll(imageList);
+    }
+
+    private void makeControls() {
+
+    }
+
+    private void makeBoard() throws IOException {
+        Image boardImage = new Image(new FileInputStream(URI_BASE + "board.png"));
+        ImageView boardIv = new ImageView(boardImage);
+        boardIv.setPreserveRatio(true);
+        boardIv.setX(Math.round((BOARD_WIDTH - boardImage.getWidth())/2));
+        this.BOARD_X = (int)boardIv.getX();
+        this.BOARD_Y = (int)boardIv.getX();
+        board.getChildren().addAll(boardIv);
+    }
+
+    private void makeControlPieces() {
+
+    }
+
     @Override
-    //To Be used in Tandem with Task 10
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("FocusGame");
+        Scene scene = new Scene(root, BOARD_WIDTH, BOARD_HEIGHT);
 
+        root.getChildren().addAll(controls, board, boardPieces, controlPieces, errors);
 
+        makeControls();
+        makeBoard();
+        makeControlPieces();
 
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
