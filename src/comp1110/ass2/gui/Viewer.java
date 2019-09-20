889ff0afa139fae06f13835afde37e8d1fd74acf
Nicholas Russell <nicholas-russell@users.noreply.github.com>
2019-09-20 19:40:33 +1000

    Removed unneeded code.

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -25,7 +25,6 @@ import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -53,7 +52,6 @@ public class Viewer extends Application {
 
     private final Group root = new Group();
     private final Group controls = new Group();
-    private final Group challengeSquare = new Group();
     private Pane pieces = new Pane();
     private Pane board = new Pane();
     private Pane errors = new Pane();
@@ -87,22 +85,19 @@ public class Viewer extends Application {
 
     /**
      * Gives an array of Piece objects from a placement string
-     * Gives an array of pieces from a placement string
-     *
      * @param placement Placement string
      * @return Array of Piece's from placement string
      */
-
     private Piece[] getPiecesFromPlacement(String placement) {
         if (!FocusGame.isPlacementStringWellFormed(placement)) {
             return null;
         }
-        int numberOfPieces = placement.length() / 4;
+        int numberOfPieces = placement.length()/4;
         int i = 0;
         int pIndex = 0;
         Piece[] pieces = new Piece[numberOfPieces];
-        while (i < numberOfPieces * 4) {
-            pieces[pIndex] = new Piece(placement.substring(i, i + 4));
+        while (i < numberOfPieces*4) {
+            pieces[pIndex] = new Piece(placement.substring(i,i+4));
             i += 4;
             pIndex++;
         }
@@ -232,7 +227,7 @@ public class Viewer extends Application {
      * @param placement A valid placement string
      */
     void makePlacement(String placement) {
-        // FIXME Task 4: implement the simple placement viewer
+        errors.getChildren().clear();
         pieces.getChildren().clear();
         if (!FocusGame.isPlacementStringWellFormed(placement)) {
             drawErrorBox("Placement string not valid");
@@ -252,136 +247,7 @@ public class Viewer extends Application {
         ImageView boardIv = new ImageView(boardImage);
         boardIv.setPreserveRatio(true);
         board.getChildren().addAll(boardIv);
-        InputStream image = getClass().getResourceAsStream("assets/a.png");
-        ImageView testPiece = new ImageView(new Image(image));
-        testPiece.setX(52);
-        testPiece.setY(93);
-        testPiece.setPreserveRatio(true);
-        testPiece.setFitHeight(130);
-
-
-        // Top left square of C-Square
-        InputStream testS = getClass().getResourceAsStream(URI_BASE + "sq-b.png");
-        ImageView ivtestS = new ImageView(new Image(testS));
-        ivtestS.setX(260);
-        ivtestS.setY(160);
-        ivtestS.setFitHeight(60);
-        ivtestS.setFitWidth(60);
-        ivtestS.setPreserveRatio(true);
-
-
-        board.getChildren().addAll(boardIv, testPiece, ivtestS);
-        Circle circle00 = new Circle(10, Color.RED);
-        circle00.setCenterX(50);
-        circle00.setCenterY(100);
-        //board.getChildren().add(circle00);
-
-
-
     }
-
-
-
-
-    /**Task 8: Placing Coloured Square png on corresponding challange square
-     * showChallenge methods add a coloured png square for C-Square under group challengeSquare
-     *
-     */
-    public void exampleSquare()  throws FileNotFoundException {
-        Image image = new Image("src/comp1110.ass2/gui/assets/sq-b.png");
-        ImageView imageView = new ImageView(image);
-        imageView.setX(300);
-        imageView.setY(300);
-        imageView.setFitHeight(60);
-        imageView.setFitWidth(60);
-        imageView.setPreserveRatio(true);
-        challengeSquare.getChildren().add(imageView);
-    }
-
-    public void makeChallengeB(int i){
-        //Coords for challenge Square 0: is approximately x = 260, y = 160, squares are not perfectly 60
-        InputStream image1 = getClass().getResourceAsStream(URI_BASE + "sq-b.png");
-        ImageView blueSquare = new ImageView(new Image(image1));
-        blueSquare.setX(challengeXCoords(i));
-        blueSquare.setY(challengeYCoords(i));
-        blueSquare.setPreserveRatio(true);
-        challengeSquare.getChildren().add(blueSquare);
-    }
-    public void makeChallengeR(int i) {
-        InputStream image2 = getClass().getResourceAsStream(URI_BASE + "sq-r.png");
-        ImageView redSquare = new ImageView(new Image(image2));
-        redSquare.setX(challengeXCoords(i));
-        redSquare.setY(challengeYCoords(i));
-        redSquare.setPreserveRatio(true);
-        challengeSquare.getChildren().add(redSquare);
-    }
-    public void makeChallengeG(int i) {
-        InputStream image3 = getClass().getResourceAsStream(URI_BASE + "sq-g.png");
-        ImageView greenSquare = new ImageView(new Image(image3));
-        greenSquare.setX(challengeXCoords(i));
-        greenSquare.setY(challengeYCoords(i));
-        greenSquare.setPreserveRatio(true);
-        challengeSquare.getChildren().add(greenSquare);
-    }
-    public void makeChallengeW(int i) {
-        InputStream image4 = getClass().getResourceAsStream(URI_BASE + "sq-w.png");
-        ImageView whiteSquare = new ImageView(new Image(image4));
-        whiteSquare.setX(challengeXCoords(i));
-        whiteSquare.setY(challengeYCoords(i));
-        whiteSquare.setPreserveRatio(true);
-        challengeSquare.getChildren().add(whiteSquare);
-    }
-    //challengeX & y Coords depending on which C-Square will return the top left coords of that specific C-Square.
-    private int challengeXCoords (int i) {
-        switch (i) {
-            case 0:
-                return 260;
-            case 1:
-                return 2;
-            case 2:
-                return 3;
-            case 3:
-                return 4;
-            case 4:
-                return 5;
-            case 5:
-                return 5;
-            case 6:
-                return 5;
-            case 7:
-                return 5;
-            case 8:
-                return 5;
-            default:
-                return 0;
-        }
-    }
-
-    private int challengeYCoords (int i) {
-        switch (i) {
-            case 0:
-                return 160;
-            case 1:
-                return 51;
-            case 2:
-                return 52;
-            case 3:
-                return 53;
-            case 4:
-                return 54;
-            case 5:
-                return 55;
-            case 6:
-                return 56;
-            case 7:
-                return 57;
-            case 8:
-                return 58;
-            default:
-                return 0;
-        }
-    }
-
 
     /**
      * Create a basic text field for input and a refresh button.
@@ -404,27 +270,19 @@ public class Viewer extends Application {
         hb.setLayoutX(130);
         hb.setLayoutY(VIEWER_HEIGHT - 50);
         controls.getChildren().add(hb);
-
     }
-
-
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("FocusGame Viewer");
         Scene scene = new Scene(root, VIEWER_WIDTH, VIEWER_HEIGHT);
 
-
-        root.getChildren().addAll(controls, board, pieces, errors, challengeSquare);
+        root.getChildren().addAll(controls, board, pieces, errors);
 
         makeControls();
         makeBoard();
-
-
 
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
-
-
