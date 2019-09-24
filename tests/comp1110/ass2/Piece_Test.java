e46d0931fad80f70d0723ca729b750991da5bb3f
Matthew <u6951444@anu.edu.au>
2019-09-24 13:24:49 +1000

    added tests for placementTOLocation and placementTOPieceType

--- tests/comp1110/ass2/Piece_Test.java
+++ tests/comp1110/ass2/Piece_Test.java
@@ -6,6 +6,7 @@ import org.junit.rules.Timeout;
 
 
 import static comp1110.ass2.TestUtility.*;
+import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class Piece_Test {
@@ -14,8 +15,18 @@ public class Piece_Test {
     @Rule
     public Timeout globalTimeout = Timeout.millis(1000);
 
-    private void placementToLocationTest(String placement, boolean expected) {
-        boolean out = Piece.placementToLocationCheck(placement);
+
+    /* Fringe Cases, valid or invalid inputs; one is valid one coord-axis and one is invalid
+    *
+     */
+    private void placementToLocationTest(String placement, Location expected) {
+        Location out = Piece.placementToLocation(placement);
+        assertEquals(expected, out);
+    }
+
+
+    private void placementToPieceTypeTest(String placement, boolean expected){
+        boolean out = Piece.placementToPieceTypeCheck(placement);
         assertTrue("Input placement string '" + placement + "', expected " + expected + " but instead got " + out, out == expected);
     }
 
@@ -23,25 +34,51 @@ public class Piece_Test {
 
     @Test
     public void placementToLocationGood() {
-        String goodTestPlacements[] = new String[]{ "a000b013c113d302e323f400g420h522i613j701",
-                "a513b130c502d002e020f401g721h101i713j332"
+
+        String goodTestPlacements[] = new String[]{
+                "a513b",
+                "a000b"
         };
 
         for(int i = 0; i < goodTestPlacements.length; i++){
-            placementToLocationTest(goodTestPlacements[i], true);
+            placementToLocationTest(goodTestPlacements[i],  new Location(5, 1));
         }
     }
 
     @Test
     public void placementToLocationBad() {
 
-        String badTestPlacements[] = new String[]{ "a000b013c113d302e323f400g420h522i613j701",
-                "a513b130c502d002e020f401g721h101i713j332"
+        String badTestPlacements[] = new String[]{
+                "a990b013c113d302e323f400g420h522i613j701",
+                "a993b130c502d002e020f401g721h101i713j332"
         };
         for(int i = 0; i < badTestPlacements.length; i++){
-            placementToLocationTest(badTestPlacements[i], false);
+            placementToLocationTest(badTestPlacements[i], new Location(0, 0) );
         }
     }
+
+    @Test
+    public void placementToPieceTypeGood(){
+        String goodTestPlacements[] = new String[]{
+                "a313",
+                "a000"
+
+        };
+        placementToPieceTypeTest(goodTestPlacements[0], true);
+        placementToPieceTypeTest(goodTestPlacements[1], true);
+    }
+
+
+    @Test
+    public void placementToPieceTypeBad(){
+        String badTestPlacements[] = new String[]{
+                "b993",
+                "c586"
+        };
+        placementToPieceTypeTest(badTestPlacements[0], false);
+    }
+
+
 }
 
 
